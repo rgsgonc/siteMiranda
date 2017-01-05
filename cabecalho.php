995f5b3c0df4d676833bd73f5a312b8c83cebed1
rgsgonc <rgs.gonc@gmail.com>
2017-01-05 16:02:27 -0200

    versão 1.0

---
+++ cabecalho.php
@@ -0,0 +1,13 @@
+<ul class="nav">
+	<li><a href="index.html"><img src="images/icon1.gif" alt="" /></a></li>
+	<li><a href="contact-us.html"><img src="images/icon2.gif" alt="" /></a></li>
+    <li class="last"><a href="#"><img src="images/icon3.gif" alt="" /></a></li>
+</ul>
+
+<ul class="site-nav">
+	<li><a href="index.php" class="act">Homeeee</a></li>
+    <li><a href="sobre.php">Sobre</a></li>
+    <li><a href="articles.php">Articles</a></li>
+    <li><a href="contato.php">Contato</a></li>
+    <li class="last"><a href="sitemap.html">Site Map</a></li>
+</ul>
